34254eb86cb200be047c7e3bda6672bacd0cf6d5
matt-pasek <m_pasek@icloud.com>
2026-07-20 23:12:47 +0200

    chore(docs): update next config and dependencies

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -2,5 +2,14 @@ import type { NextConfig } from "next";
 
 const config: NextConfig = {
   transpilePackages: ["@matt-pasek/usva", "@matt-pasek/usva-tokens"],
+  async redirects() {
+    return [
+      {
+        source: "/tokens",
+        destination: "/design-language/color",
+        permanent: true,
+      },
+    ];
+  },
 };
 export default config;
